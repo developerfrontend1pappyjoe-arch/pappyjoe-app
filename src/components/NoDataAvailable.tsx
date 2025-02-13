cde5c1fbc707717660356dac364b0398caa05018
BINIL E B <binil@pappyjoe.com>
2025-02-13 13:01:50 +0530

    Add Lottie animations for loading and no data states

--- src/components/NoDataAvailable.tsx
+++ src/components/NoDataAvailable.tsx
@@ -1,32 +1,38 @@
+import React, { useRef } from "react"
 import {View, Text, StyleSheet, Image, Dimensions} from 'react-native';
 import {colorList} from '../styles/global.styles';
 import {NoDataImage} from '../assets';
+import LottieView from "lottie-react-native";
 
 export const NoDataAvailable = () => {
+  const animation = useRef(null);
   return (
-    <View style={styles.constainer}>
-      {/* <Text style={styles.text}>No Data Available</Text> */}
-
-      <Image
-        source={NoDataImage}
-        style={{
-          resizeMode: 'contain',
-          width: 200,
-          height: 200,
-        }}
-      />
+    <View style={styles.container}>
+       <LottieView
+             autoPlay
+             ref={animation}
+             style={styles.animation}
+             source={require('../assets/lottie_files/empty_data.json')}
+           />
+           <Text style={styles.text}>No data</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  constainer: {
-    flex: 1,
-    justifyContent: 'center',
+  container: {
     alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
   },
-  text: {
-    fontSize: 20,
-    color: colorList.dark,
+  animation: {
+    width: 200,
+    height: 200,
+    padding:0,
+    margin:0,
   },
+  text:{
+     fontSize:20,
+     fontWeight:"bold"
+  }
 });
