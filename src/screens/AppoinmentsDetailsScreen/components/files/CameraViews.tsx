e743c7fe0cae73632d3467d2c21c7eff1e0d90cc
BINIL E B <binil@pappyjoe.com>
2025-03-13 14:14:46 +0530

    Add zoom functionality to CameraViews component; integrate slider for zoom control and improve permission handling

--- src/screens/AppoinmentsDetailsScreen/components/files/CameraViews.tsx
+++ src/screens/AppoinmentsDetailsScreen/components/files/CameraViews.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { useEffect, useRef, useState } from "react";
-import {
-  Camera,
-  useCameraDevice,
-} from "react-native-vision-camera";
+import { Camera, useCameraDevice } from "react-native-vision-camera";
 import {
   Dimensions,
   PermissionsAndroid,
@@ -16,28 +13,29 @@ import ImageViewer from "react-native-image-zoom-viewer";
 import { Button, Icon, Surface, Text, TextInput } from "react-native-paper";
 import RNFS from "react-native-fs";
 import { colorList } from "../../../../styles/global.styles";
+import Slider from "@react-native-community/slider";
 
 export const CameraViews = ({ setImageFiles }: any) => {
   const device = useCameraDevice("back");
   const cameraRef = useRef(null);
   const [photoUri, setPhotoUri] = useState(null);
   const [imageFileName, setImageFileName] = useState("");
-  const [showPermisionBtn,setShowPermisionBtn] = useState(true)
-  const [imgExtention,setImgExtention] = useState<string>("")
+  const [showPermisionBtn, setShowPermisionBtn] = useState(true);
+  const [imgExtention, setImgExtention] = useState<string>("");
+  const [zoomValue, setZoomValue] = useState<number>(1);
   async function requestCameraPermission() {
-    
     try {
       const granted = await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.CAMERA,
         {
-          title: 'Camera Permission',
-          message: 'App needs access to your camera.',
-          buttonPositive: 'OK',
-        },
+          title: "Camera Permission",
+          message: "App needs access to your camera.",
+          buttonPositive: "OK",
+        }
       );
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
         // console.log("clicked----------------->");
-        setShowPermisionBtn(false)
+        setShowPermisionBtn(false);
       } else {
         // console.log('Camera permission denied');
       }
@@ -45,7 +43,6 @@ export const CameraViews = ({ setImageFiles }: any) => {
       console.warn(err);
     }
   }
-
 
   const takePicture = async () => {
     if (cameraRef !== null) {
@@ -80,49 +77,62 @@ export const CameraViews = ({ setImageFiles }: any) => {
         size: size,
         type: `image/${type}`,
       };
-      const splitedName = name.split('.')
-      setImageFileName(splitedName[0])
-      setImgExtention(splitedName[1])
+      const splitedName = name.split(".");
+      setImageFileName(splitedName[0]);
+      setImgExtention(splitedName[1]);
       setPhotoUri(fileObj);
     } catch (error) {
       console.error("Error:-------------->", error);
     }
   };
 
+
   const PendingView = () => {
     return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap:15
-      }}>
-      <Text>No permission has been granted for the camera.</Text>
-      <Button mode="contained-tonal" onPress={requestCameraPermission}>
-      Grant Permission
-      </Button>
-    </View>
-  )};
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 15,
+        }}
+      >
+        <Text>No permission has been granted for the camera.</Text>
+        <Button mode="contained-tonal" onPress={requestCameraPermission}>
+          Grant Permission
+        </Button>
+      </View>
+    );
+  };
 
-  const checkPermision = async()=>{
+  const checkPermision = async () => {
     const granted = await PermissionsAndroid.check(
       PermissionsAndroid.PERMISSIONS.CAMERA
-    )
-    if(!granted){
-      requestCameraPermission()
+    );
+    if (!granted) {
+      requestCameraPermission();
     }
-    setShowPermisionBtn(!granted)
-  }
+    setShowPermisionBtn(!granted);
+  };
+
   useEffect(() => {
-    checkPermision()
+    checkPermision();
+    return () => {
+      setZoomValue(0.1);
+    };
   }, [device]);
 
   if (showPermisionBtn) {
-    return <PendingView />
-  }else{
+    return <PendingView />;
+  } else {
     return (
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          width: Dimensions.get("screen").width,
+          height: Dimensions.get("screen").height,
+        }}
+      >
         {photoUri ? (
           <View style={{ flex: 1, width: Dimensions.get("screen").width * 1 }}>
             <ImageViewer
@@ -153,65 +163,78 @@ export const CameraViews = ({ setImageFiles }: any) => {
               </Button>
             </View>
           </View>
-        ) : (
+        ) : 
+         (
           <>
-            <Camera
-              ref={cameraRef}
-              photo={true}
-              style={{
-                flex: 1,
-                width: Dimensions.get("screen").width,
-                height: Dimensions.get("screen").height * 0.5,
-                position: "relative",
-              }}
-              device={device}
-              isActive={true}
-              enableZoomGesture
-              resizeMode="contain"
-              enableHighQualityPhotos
-            />
-            <TouchableOpacity
-              onPress={takePicture}
-              style={
-                Platform.OS === "ios"
-                  ? {
-                      position: "absolute",
-                      bottom: 30,
-                      left: "40%",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }
-                  : {
-                      position: "absolute",
-                      bottom: 50,
-                      left: "40%",
-                      // backgroundColor: colorList.primary,
-                      width: 80,
-                      height: 80,
-                      borderRadius: 100,
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }
-              }
-            >
-              <Icon source={"camera-iris"} color="white" size={80} />
-              {Platform.OS === "ios" ? (
-                <Surface
-                  elevation={5}
-                  style={{
-                    backgroundColor: "red",
-                    width: 80,
-                    height: 80,
-                    borderRadius: 100,
-                  }}
-                />
-              ) : null}
-            </TouchableOpacity>
-          </>
-        )}
+    <Camera
+      ref={cameraRef}
+      photo={true}
+      style={{
+        flex: 1,
+        width: Dimensions.get("screen").width,
+        height: Dimensions.get("screen").height,
+        // position: "relative",
+      }}
+      device={device}
+      isActive={true}
+      minZoom={1}
+      zoom={zoomValue}
+      enableZoomGesture
+      resizeMode="cover"
+      enableHighQualityPhotos
+    />
+    <View
+      style={
+        Platform.OS === "ios"
+          ? {
+              position: "absolute",
+              bottom: 30,
+              left: "40%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }
+          : {
+              position: "absolute",
+              bottom: 50,
+              left: "40%",
+              // backgroundColor: colorList.primary,
+              width: 80,
+              height: 80,
+              borderRadius: 100,
+              justifyContent: "center",
+              alignItems: "center",
+            }
+      }
+    >
+      <Slider
+        value={zoomValue}
+        onValueChange={(value) => { setZoomValue(value);}}
+        style={{ width: 200, height: 40 }}
+        minimumValue={1}
+        maximumValue={16}
+        minimumTrackTintColor="#FFFFFF"
+        maximumTrackTintColor="#000000"
+      />
+      <TouchableOpacity onPress={takePicture}>
+        <Icon source={"camera-iris"} color="white" size={80} />
+        {Platform.OS === "ios" ? (
+          <Surface
+            elevation={5}
+            style={{
+              backgroundColor: "red",
+              width: 80,
+              height: 80,
+              borderRadius: 100,
+            }}
+          />
+        ) : null}
+      </TouchableOpacity>
+    </View>
+  </>
+         )
+        }
       </SafeAreaView>
     );
   }
-
 };
